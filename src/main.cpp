cf5ee75689e81ea9a49fb995b9902acff4dfb9a7
mathis.haba <mathis1.haba@epitech.eu>
2025-12-27 18:53:41 +0100

    [~] Implement packet counting and channel scanning in the main loop

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include <esp_wifi.h>
 #include <Arduino.h>
 #include <WiFi.h>
 #include <Wire.h>
@@ -10,6 +11,12 @@
 
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
+volatile int packet_counter = 0;
+int current_channel = 1;
+
+void promiscuous_rx_cb(void* buf, wifi_promiscuous_pkt_type_t type) {
+  packet_counter++;
+}
 
 void setup() {
   Serial.begin(115200);
@@ -23,22 +30,41 @@ void setup() {
   display.setTextColor(WHITE);
   display.setTextSize(1);
   display.setCursor(0, 0);
-
   display.println("Sentinel System");
   display.println("Starting...");
-
   display.display();
 
-  Serial.println("Sentinel Ready");
+  WiFi.mode(WIFI_STA);
+  WiFi.disconnect();
 
-  delay(5000);
-  display.clearDisplay();
-  display.display();
+  esp_wifi_set_promiscuous(true);
+  esp_wifi_set_promiscuous_rx_cb(&promiscuous_rx_cb);
+
+  delay(1000);
 }
 
 void loop() {
-  Serial.println("System alive...");
-  delay(1000);
-  Serial.println("System still alive...");
-  delay(1000);
+  display.clearDisplay();
+  display.setCursor(0, 0);
+  display.println("STATUS: SCANNING");
+  display.println("~~~~~~~~~~~~~~~~");
+  display.print("Packets:   ");
+  display.println(packet_counter);
+  display.print("Channel: ");
+  display.println(WiFi.channel());
+  display.display();
+
+  if (packet_counter > 10000){
+    packet_counter = 0;
+  }
+
+  if (current_channel <= 13) {
+    esp_wifi_set_channel(current_channel, WIFI_SECOND_CHAN_NONE);
+    current_channel++;
+  }
+  else {
+    current_channel = 1;
+  }
+
+  delay(200);
 }
